7d8ffb031ce5745c06b5276dbd1c913497b74276
yoyorblx4-design <yoyo.rblx4@gmail.com>
2025-10-08 13:52:00 +0100

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,4 +1,5 @@
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 local player = Players.LocalPlayer
 
 local whitelist = {
@@ -6,9 +7,23 @@ local whitelist = {
     9670086844
 }
 
-if not table.find(whitelist, player.UserId) then
-    player:Kick("You are not authorized to use this script.")
-    return
+local function checkWhitelist()
+	if not table.find(whitelist, player.UserId) then
+		player:Kick("You are not authorized to use this script.")
+		return
+	end
+	print("Access granted ✅")
 end
+checkWhitelist()
 
-print("Access granted ✅")
+RunService.Heartbeat:Connect(function(step)
+	if not _G.whitelistCheckTimer then
+		_G.whitelistCheckTimer = 0
+	end
+
+	_G.whitelistCheckTimer = _G.whitelistCheckTimer + step
+	if _G.whitelistCheckTimer >= 60 then
+		_G.whitelistCheckTimer = 0
+		checkWhitelist()
+	end
+end)
